4a5192e2d72ae3ff3b3079a590470f77ffbc3b74
akotoernest <akotoernest48@gmail.com>
2023-02-18 03:14:06 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 /**
@@ -7,7 +8,7 @@
  */
 
 int main(void)
-{
-         puts("\"Programming is like building a multilingual puzzl"):
-         return (0):
+{		
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
