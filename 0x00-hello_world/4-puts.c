3ce3b31e7422ee884bc6a57d956d30645c333156
akotoernest <akotoernest48@gmail.com>
2023-02-18 02:24:43 -0800

    name

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is my first function
+ * 
+ * Return: always 0
+ */
+
+int main(void)
+{
+         puts("\"Programming is like building a multilingual puzzl"):
+         return (0):
+}
