b3d42691095e4091fe77b3a6b373cba8be1954ad
akotoernest <akotoernest48@gmail.com>
2023-02-18 03:28:50 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 
 #include <stdio.h>
-
 /**
- * main - this is my first function
- * 
+ *main - this is my first function
+ *
  * Return: always 0
  */
 
 int main(void)
-{		
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
